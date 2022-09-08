2ffdbf63a7b45f5bafd01de2e395dcb48acc3af6
AndresHelloWorld <andresangg@gmail.com>
2022-09-07 22:12:42 -0500

    add

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const RandomOrg = require('random-org');
-let table = require("table");
 var data, sum = 0
-var random = new RandomOrg({ apiKey: 'd9230aa5-0aea-4cf3-b479-ac7c222ff0ed' });
-var sumaFr = 0, raz = Math.sqrt(10000), DISTRINORMALINV = 0.509972518, mErr = 0.05
+var random = new RandomOrg({ apiKey: '0e1fa5c6-89de-4ad7-a85a-c59e90915920' });
+var distriNormalInv = 0.509972518, mErr = 0.05
 const K = 1.33 * Math.log(10000), Kre = Math.round(K) + 1, amplitud = Math.round(100 / Kre)
 //752220f3-fd99-4ee4-97a3-b4c782fff258
 //a2fe8d15-b03b-43f1-95b9-b93473946204
@@ -21,25 +20,19 @@ const fo = (LS, LI) => {
 }
 
 const frecuen = () => {
-  config = {
-  
-    // Predefined styles of table
-    border: table.getBorderCharacters("ramac"),
-  }
   var i = 1
   var lI = 0, lS = amplitud, fE
-  var message = `Li       Ls            Fe            Fo      err\n`
+  var table = `Li       Ls            Fe            Fo      err\n`
   while (i != Kre + 1) {
     lI = i > 1 ? lS : 0
     lS = i == 13 ? (100 / Kre) * i : amplitud * i
     fE = (10000 / Kre).toFixed(4)
     var error = Math.pow(((fE-fo(lS, lI))/fE),2)
     
-
-    message += `${lI}     ${lS}      ${fE}      ${fo(lS, lI)}     ${error.toFixed(6)}\n`
+    table += `${lI}     ${lS}      ${fE}      ${fo(lS, lI)}     ${error.toFixed(6)}\n`
     i++
   }
-  return message
+  return table
 }
 
 data = random.generateIntegers({ min: 0, max: 100, n: 10000 })
@@ -47,16 +40,13 @@ data = random.generateIntegers({ min: 0, max: 100, n: 10000 })
     data = result.random.data
     for (var i = 0; i < data.length; i++) {
       sum += data[i]
-      // console.log(data[i])
     }
     var prom = sum / data.length
     var cal = (0.5-prom)*(Math.sqrt(10000)/Math.sqrt(1/12))
-    var z = (Math.abs(DISTRINORMALINV) <= 1) ? "Aceptada": "Rechazada"
+    var z = (Math.abs(distriNormalInv) <= cal) ? "Aceptada": "Rechazada"
     console.log(frecuen())
-    console.log("K aprox: ", Kre)
-    console.log("Amplitud: ", amplitud)
-    console.log("El promedio es ", prom, "\nLamnda ", cal, "\nmanejo error(%): 5% -> ", mErr,"\nZm ", z)
+    console.log("K aprox: ", Kre, "\nAmplitud: ", amplitud, "\nPromedio: ", prom, "\nLambda: ", cal, "\nmanejo error(%): 5% -> ", mErr,"\nZ: ", z)
   }).catch((error)=>{
-    console.error("Error, cambiar la apiKey -->", error.message)
+    console.error("Cambiar la apiKey -->", error.message)
   })
 
